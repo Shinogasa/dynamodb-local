4502707ec671337b168732aaa91fdfb2d28bdf1f
Shinonome <cuulbulu@gmail.com>
2020-11-18 23:55:26 +0900

    Update: python実行のコンテナ作成

--- README.md
+++ README.md
@@ -4,3 +4,25 @@
 
 - <https://qiita.com/gzock/items/e0225fd71917c234acce>
 - <https://developers.freee.co.jp/entry/dynamodb-local>
+- <https://techacademy.jp/magazine/47408>
+- <https://qiita.com/ekzemplaro/items/62196519c70515bc5ec8>
+- <https://qiita.com/ekzemplaro/items/82b2013bd753ea981173>
+- <https://www.tacoskingdom.com/blog/27>
+
+## dynamodb localのコンテナIP
+
+```bash
+docker ps -a
+
+docker inspect --format='{{range .NetworkSettings.Networks}}{{.Gateway}}{{end}}' <CONTAINER ID>
+```
+
+## use
+
+```bash
+docker-compose up -d --build
+
+docker-compose exec python3 bash
+
+docker-compose down
+```
---
+++ create-table.py
@@ -0,0 +1,29 @@
+import boto3
+
+dynamodb = boto3.resource('dynamodb', 
+                          endpoint_url="http://172.24.0.1:8000",
+                          region_name='us-west-2',
+                          aws_access_key_id='fakeMyKeyId',
+                          aws_secret_access_key='fakeSecretAccessKey')
+
+table = dynamodb.create_table(
+    TableName='users',
+    KeySchema=[
+        {
+            'AttributeName': 'user_id',
+            'KeyType': 'HASH'
+        }
+    ],
+    AttributeDefinitions=[
+        {
+            'AttributeName': 'user_id',
+            'AttributeType': 'N'
+        }
+    ],
+    ProvisionedThroughput={
+        'ReadCapacityUnits': 5,
+        'WriteCapacityUnits': 5
+    }
+)
+
+print('Table status:', table.table_status)
